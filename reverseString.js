823265526d4ea7e871412399b987a8b8c659b920
rauxw <hardikraut909@gmail.com>
2025-05-18 15:57:01 +0530

    reverse string

---
+++ reverseString.js
@@ -0,0 +1,12 @@
+const originalString = "Apple";
+
+function reverseString(string) {
+  let reverse = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverse += string[i];
+  }
+  return reverse;
+}
+
+const reverseStringResult = reverseString(originalString);
+console.log(reverseStringResult);
